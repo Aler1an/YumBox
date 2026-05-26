dd4b42e10ae643f9dd7de02cc4891a8d6f4f62c9
Maksim Yatsenyuk <yaesenykmax356@gmail.com>
2026-05-26 13:03:13 +0300

    fix(ui): fix layout

--- src/components/layout/Basket/BasketFooter.jsx
+++ src/components/layout/Basket/BasketFooter.jsx
@@ -12,8 +12,8 @@ const BasketFooter = props => {
 	const { deliveryPrice, finalPrice, onCheckout } = props
 
 	return (
-		<div className={cn(flexCol, 'bg-[#222] rounded-t-[31px] px-7.25 py-5.5 pb-6.5')}>
-			<div className={cn(flexBetween, 'mb-6.5 items-end')}>
+		<div className={cn(flexCol, 'bg-background rounded-t-[31px] px-7.25 py-5.5 pb-6.5')}>
+			<div className={cn(flexBetween, 'mb-6.5 items-center')}>
 				<span className={cn(textLgSemibold, 'font-avenir')}>Доставка</span>
 				<span className={cn(textSemibold, 'font-avenir text-[26px]')}>{deliveryPrice} ₴</span>
 			</div>
@@ -25,9 +25,9 @@ const BasketFooter = props => {
 				)}
 				onClick={onCheckout}
 			>
-				<span className="absolute inset-0 rounded-full bg-[#fcb852]" />
+				<span className="absolute inset-0 rounded-full bg-primary" />
 				<span className="absolute inset-0 bg-black scale-x-0 origin-left transition-transform duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-x-100" />
-				<span className="absolute inset-0 rounded-full border-2 border-[#fcb852] pointer-events-none opacity-0 transition-opacity duration-500 delay-300 ease-out group-hover:opacity-100" />
+				<span className="absolute inset-0 rounded-full border-2 border-primary pointer-events-none opacity-0 transition-opacity duration-500 delay-300 ease-out group-hover:opacity-100" />
 				<span className={cn(transitionColors, 'relative z-10 group-hover:text-white')}>
 					Оформити за {finalPrice.toFixed(0)} ₴
 				</span>
--- src/components/layout/MobileMenu/MobileMenu.jsx
+++ src/components/layout/MobileMenu/MobileMenu.jsx
@@ -16,22 +16,24 @@ const MobileMenu = props => {
 			<div
 				className={cn(
 					transitionTransform,
-					'fixed top-20 left-0 right-0 bottom-0 bg-black z-50 md:hidden overflow-hidden transform',
+					'fixed top-20 left-0 right-0 bottom-0 bg-black z-50 md:hidden overflow-y-auto transform',
 					isOpen ? 'translate-x-0' : 'translate-x-full'
 				)}
 			>
-				<BasketButton
-					className={cn(basketButton, 'w-fit mx-16 mb-4.75 mt-11.75')}
-					onBasketClick={onBasketClick}
-					basketItemsCount={basketItemsCount}
-					totalPrice={totalPrice}
-				/>
+				<div className={cn(flexCol, 'mt-11.75 mx-16 pb-10')}>
+					<BasketButton
+						className={cn(basketButton, 'w-fit mb-4.75 ')}
+						onBasketClick={onBasketClick}
+						basketItemsCount={basketItemsCount}
+						totalPrice={totalPrice}
+					/>
 
-				<nav className="px-16 mb-5.5">
-					<Navigation linkClassName="text-white text-2xl font-medium leading-[246%]" />
-				</nav>
-				<Contacts className={cn(flexCol, 'justify-center px-16.5 gap-2.5 mb-18.75')} />
-				<Socials className="flex gap-11 pl-17.75" />
+					<nav className="mb-5.5">
+						<Navigation linkClassName="text-white text-2xl font-medium leading-[246%]" />
+					</nav>
+					<Contacts className={cn(flexCol, 'justify-center pl-0.5 gap-2.5 mb-18.75')} />
+					<Socials className="flex gap-11 pl-1.75" />
+				</div>
 			</div>
 		</>
 	)
