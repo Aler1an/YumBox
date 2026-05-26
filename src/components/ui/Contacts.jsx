72d7745f7ec6da0c6ccc03e36c6e60937209f9dd
Maksim Yatsenyuk <yaesenykmax356@gmail.com>
2026-05-26 12:45:56 +0300

    style(ui): add coherent hover opacity and underline for contacts and style global scrollbar

--- src/components/ui/Contacts.jsx
+++ src/components/ui/Contacts.jsx
@@ -1,5 +1,5 @@
 import { cn } from '@lib/cn'
-import { textLgSemibold, transitionColors } from '@styles/ui'
+import { textLgSemibold, underlineEffect, transitionOpacity } from '@styles/ui'
 
 const Contacts = props => {
 	const { className = '' } = props
@@ -7,17 +7,13 @@ const Contacts = props => {
 	return (
 		<div className={className}>
 			<a
-				className={cn(
-					textLgSemibold,
-					transitionColors,
-					'text-[#f3ba4f] cursor-pointer hover:text-white'
-				)}
+				className={cn(textLgSemibold, underlineEffect, transitionOpacity, 'text-secondary cursor-pointer md:opacity-85 hover:opacity-100')}
 				href="mailto:yumbox.lutsk@gmail.com"
 			>
 				yumbox.lutsk@gmail.com
 			</a>
 			<a
-				className={cn(textLgSemibold, transitionColors, 'cursor-pointer hover:text-[#f3ba4f]')}
+				className={cn(textLgSemibold, underlineEffect,transitionOpacity, 'text-white cursor-pointer md:opacity-85 hover:opacity-100')}
 				href="tel:+380 93 823 92 93"
 			>
 				+380 93 823 92 93
